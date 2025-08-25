5f553f7403cd8478b4a431cfa256760a037e2f98
ganyuo <ganyuo@outlook.com>
2025-08-25 11:14:17 +0800

    添加mpu6500驱动代码

---
+++ Core/Src/device/mpu6500.c
@@ -0,0 +1,7 @@
+#include "mpu6500.h"
+
+void mpu6500_send_data();
+void mpu6500_recvice_data();
+void mpu6500_init();
+void mpu6500_read_register();
+void mpu6500_write_register();
---
+++ Core/Src/device/mpu6500.h
@@ -0,0 +1,12 @@
+
+#ifndef __MPU_6500_H__
+#define __MPU_6500_H__
+
+void mpu6500_send_data();
+void mpu6500_recvice_data();
+void mpu6500_init();
+void mpu6500_read_register();
+void mpu6500_write_register();
+
+
+#endif /* __MPU_6500_H__ */
